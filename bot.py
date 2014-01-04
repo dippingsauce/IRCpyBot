1c33a29fa48e7a6f9be56aa484faa9a9f147c2f6
Pepe Sanchez <dippingsaucespm@gmail.com>
2014-01-04 02:56:11 -0800

    Fixed Connect issues
    it connects and stays connected now and i added in a nickname in user
    error

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 import sys
 import socket
 import string
+import errno
+import threading
 
 from configs.config import CONN, USER
 
+class NicknameInUseError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+    
 def parsemsg(msg):
     complete=msg[1:].split(':',1) #Pase the message into useful data
     info=complete[0].split(' ')
@@ -14,34 +22,36 @@ def parsemsg(msg):
         cmd = msgpart[1:].split(' ')
 
 def run(conn):
-    irc = socket.socket() #creates a socket
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a socket
     irc.connect(conn) #connects to the server
     
-
-    while 1:                
-                line = irc.recv(1024) #recieves server messages
-                if line: # only print meaningful stuff
-                        print line #server message output
-                        if line[0].find('PING')!= -1:
-                                print 'Sending PONG'
-                                parsemsg(line)
-                                line=line.rstrip() #removes trailing 'rn'
-                                line=line.split()
-                                if(line[0] == 'PING'): #if server pings then pong
-                                        irc.send('PONG '+line[1]+'\r\n')        
-                        if line[0].find('Found')!= -1:
-                                print 'Sending IDENT'    
-                                irc.send('NICK ' + USER['nick'] + '\n') #sends nick to the server
-                                irc.send('USER %s 0 0 :%s\n' % (USER['ident'], USER['realname'])) #identify with the server
-                                irc.send('JOIN #empornium\n') #Joins default channel
-                        if line[0].find('ERROR')!= -1:
-                                print 'That\'s an error'
-                                irc.close()
-                                sys.exit() #Die on errors                        
-                        if line[0].find('PRIVMSG') != -1: #channel joined now parse messages
-                                parsemsg(line)
-                                line=line.rstrip() #removes trailing 'rn'
-                                line=line.split()
+    while 1:
+        line = irc.recv(1024) #recieves server messages
+        if line: # only print meaningful stuff
+            print line #server message output
+            if line.find('Found your hostname')!= -1:
+                print 'Sending IDENT'    
+                irc.send('NICK ' + USER['nick'] + '\n') #sends nick to the server
+                irc.send('USER %s 0 0 :%s\n' % (USER['ident'], USER['realname'])) #identify with the server
+            elif line.find('PING :')!= -1:
+                print 'Sending PONG'
+                parsemsg(line)
+                line=line.rstrip() #removes trailing 'rn'
+                line=line.split()
+                if(line[0] == 'PING'): #if server pings then pong
+                    irc.send('PONG '+line[1]+'\r\n')
+            elif line.find('Nickname is already in use') != -1:
+                raise NicknameInUseError("The Nickname you chose is already in use.")
+            elif line.find('ERROR')!= -1:
+                print "That's an error"
+                irc.close()
+                sys.exit() #Die on errors                        
+            elif line.find('PRIVMSG') != -1: #channel joined now parse messages
+                parsemsg(line)
+                line=line.rstrip() #removes trailing 'rn'
+                line=line.split()
+                print line
+                
 
 if __name__ == "__main__":
     conn = (CONN['host'], CONN['port'])
