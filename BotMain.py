5c147fa20f0b4c9cecaa4a107ad0d9473dbf693b
ValliusDax <ValliusDax@Deep.Space.9>
2014-01-04 17:57:49 +0000

    Semi-working UI code now.

--- BotMain.py
+++ BotMain.py
@@ -1,17 +1,25 @@
 import BotClass
+import Queue
 from BotClass import *
+from BotUI import *
 
+        
 class Bot(BotClass):
     def __init__(self):
         BotClass.__init__(self)
+        try:
+           IRC_THREAD = threading.Thread(target=self.connect)
+           IRC_THREAD.daemon = True
+           IRC_THREAD.start()
+        except NicknameInUseError as e:
+            print e.value
+        except KeyboardInterrupt:
+            print "Closing"
+        ui = BotUI(super(Bot,self).ui_console_queue)
 
     def connect(self):
         super(Bot,self).connect()
 
 if __name__ == "__main__":
-    b = Bot()
-    try:
-        b.connect()
-    except NicknameInUseError as e:
-        print e.value
+    irc = Bot()
     
--- BotUI.py
+++ BotUI.py
@@ -1,39 +1,62 @@
 import curses
 import Queue
+import threading
+import sys
 
 class BotUI(object):
-    
-    def main(self,screen,irc_print_queue):
 
-        curses.start_color() 
-        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE) 
-        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK) 
+    def main(self,screen,ui_print_queue):
+        try:
+            curses.start_color()
+            curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE)
+            curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
 
-        screen.bkgd(curses.color_pair(1)) 
-        screen.refresh() 
+            screen.bkgd(curses.color_pair(1))
+            screen.refresh()
 
-        win_size = screen.getmaxyx()
-        win = curses.newwin(win_size[0], win_size[1], 0, 0) 
-        win.bkgd(curses.color_pair(2)) 
-        win.box() 
-        
-        
-        #while True:            
-        win.addstr(2, 2, irc_print_queue.get()) 
-        win.refresh() 
-        c = screen.getch() 
+            win_size = screen.getmaxyx()
+            con_win = curses.newwin(win_size[0], (win_size[1]/2), 0, 0)
+            con_win.bkgd(curses.color_pair(2))
+            con_win.box()
+            con_win.addstr(0, ((win_size[1]/4)-6), "Bot Console")
+            con_win.move(1,1)
+            con_line = 1
+            con_lines = con_line
+            con_max_line_len = ((win_size[1]/2)-8)
+            con_max_lines = (win_size[0]-2)
 
-    
-    def __init__(self,irc_print_queue):
-        
-        try: 
-             curses.wrapper(self.main, irc_print_queue) 
-        except KeyboardInterrupt: 
-             print "Got KeyboardInterrupt exception. Exiting..." 
-             exit()
-                
-if __name__ == "__main__":
-    irc_print_queue = Queue.Queue(maxsize=0)
-    b = BotUI(irc_print_queue)
-    irc_print_queue.put('Hello World\n')
+            while True:
+                con_win.box()
+                con_win.addstr(0, ((win_size[1]/4)-6), "Bot Console")
+                if ui_print_queue.empty() == False:
+                    line=ui_print_queue.get()
+                    line_len=len(line)
+                    start=0
+
+                    for i in range(0,(line_len/con_max_line_len)):
+                        end=min(line_len, (con_max_line_len*(i+1)))
+                        if con_line >= con_max_lines:
+                            con_win.move(1,2)
+                            con_win.insdelln(-1)
+                            con_win.move(con_line,1)
+                            con_win.clrtoeol()
+                            
+                        else:
+                            con_line+=1
+
+                        con_win.addstr((con_line-1), 2, line[start:end])
+                        start = end
+                        con_win.move(con_line,2)
+                con_win.refresh()
+        except Exception as e:
+            print e
+            exit()
+
+
+    def __init__(self,ui_print_queue):
+        try:
+            curses.wrapper(self.main,ui_print_queue)
+        except KeyboardInterrupt:
+            print "Closing"
+            exit()
